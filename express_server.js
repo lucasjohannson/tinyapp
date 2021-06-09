2c29461b4919d0c3152da90ecde39bf38fc8315a
lucas <lucas.johannson1@ucalgary.ca>
2021-06-09 00:42:41 +0000

    log in log out feature using cookies

--- express_server.js
+++ express_server.js
@@ -3,7 +3,11 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
 
 function generateRandomString() {
   let shortUrl = Math.random().toString(36).substring(7);
@@ -35,16 +39,18 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
+  console.log(templateVars);
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
@@ -64,6 +70,17 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.new;
   console.log(urlDatabase);  // Log the POST request body to the console 
+  res.redirect("/urls");
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  res.cookie('username', req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username')
   res.redirect("/urls");
 });
 
